ff4b6b5fbe2be9145c3199f201697e0d442cc185
Gerson <contacto.gerson.mexico@gmail.com>
2020-04-11 19:29:36 -0500

    unidad 2 actividad 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      
-      home: Scaffold(
-        appBar: AppBar(title: Text("titulo"),) ,
-        body:
-          Center(
-            child: Text('Gerson Eliezer Morales Hdz', textScaleFactor: 2.5,),
+        home: Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Flexible(
+            child: Container(color: Colors.blue, height: 100, width: 100),
           ),
-      )
-    );
+          Flexible(
+            child: Container(color: Colors.red, height: 200),
+          ),
+          Flexible(
+            child: Container(color: Colors.amber, height: 100,),
+          )
+        ],
+      ),
+    ));
   }
 }
